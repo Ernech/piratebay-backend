50b017f24a977eef2abb0d916304f769640ef318
Ernesto Vilela Montero <ernesto.vilela.montero@gmail.com>
2020-03-26 01:38:28 -0400

    Client adder

--- src/main/java/com/ernesto/backend/ClientController.java
+++ src/main/java/com/ernesto/backend/ClientController.java
@@ -4,6 +4,7 @@ import ch.qos.logback.core.net.server.Client;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +14,16 @@ import java.util.Date;
 @RestController
 @RequestMapping("/api/v1/clients")
 public class ClientController {
+
         @RequestMapping(
                 method = RequestMethod.POST,
                 produces = MediaType.APPLICATION_JSON_VALUE,
                 consumes = MediaType.APPLICATION_JSON_VALUE)
-        public ResponseEntity<ClientDto> helloWorld(ClientDto clientDto){
+
+        public ResponseEntity<ClientDto> createClient(@RequestBody ClientDto clientDto){
             System.out.println(clientDto);
             clientDto.setBirthDate(new Date());
-            return new ResponseEntity<ClientDto>(clientDto, HttpStatus.OK);
+            return new ResponseEntity<>(clientDto, HttpStatus.OK);
 
     }
 }

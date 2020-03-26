f2808a07960396a55835afa4dd0f6d0b072e8668
Ernesto Vilela Montero <ernesto.vilela.montero@gmail.com>
2020-03-26 01:07:53 -0400

    ClientController fixed

--- src/main/java/com/ernesto/backend/ClientController.java
+++ src/main/java/com/ernesto/backend/ClientController.java
@@ -8,18 +8,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 @RequestMapping("/api/v1/clients")
 public class ClientController {
-    public class TestController {
         @RequestMapping(
                 method = RequestMethod.POST,
                 produces = MediaType.APPLICATION_JSON_VALUE,
                 consumes = MediaType.APPLICATION_JSON_VALUE)
         public ResponseEntity<ClientDto> helloWorld(ClientDto clientDto){
             System.out.println(clientDto);
+            clientDto.setBirthDate(new Date());
             return new ResponseEntity<ClientDto>(clientDto, HttpStatus.OK);
-        }
-    }
 
+    }
 }

5af9c7d7d61fa6fac64fb8b01901b0062757f06c
Ernesto Vilela Montero <ernesto.vilela.montero@gmail.com>
2020-03-26 23:45:57 -0400

    Altas modificaciones y bajas

--- src/main/java/com/ernesto/backend/ClientController.java
+++ src/main/java/com/ernesto/backend/ClientController.java
@@ -5,12 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/clients")
@@ -28,11 +27,62 @@ public class ClientController {
                 produces = MediaType.APPLICATION_JSON_VALUE,
                 consumes = MediaType.APPLICATION_JSON_VALUE)
 
-        public ResponseEntity<ClientDto> createClient(@RequestBody ClientDto clientDto){
-            //System.out.println(clientDto);
-            //clientDto.setBirthDate(new Date());
+        public ResponseEntity createClient(@RequestBody ClientDto clientDto){
+            //Verificamos que el firstName no sea nulo o vacio
+            if(clientDto.getFirstName()==null || clientDto.getFirstName().trim().equals("")){
+                Map response = new HashMap();
+                response.put("code",1000);
+                response.put("Mmessage","El cliente no tiene primer nombre");
+                return new ResponseEntity<Map>(response,HttpStatus.BAD_REQUEST);
+            }
+            //Verificamos que el lastName no sea nulo o vacio
+            if(clientDto.getLastName()==null || clientDto.getLastName().trim().equals("")){
+                Map response = new HashMap();
+                response.put("code",1000);
+                response.put("Mmessage","El cliente no tiene apellidos");
+                return new ResponseEntity<Map>(response,HttpStatus.BAD_REQUEST);
+            }
             clientMapper.create(clientDto);
             return new ResponseEntity<>(clientDto, HttpStatus.OK);
 
+    }
+
+    @RequestMapping(method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity listClient(){
+        return new ResponseEntity<>(clientMapper.listAll(),HttpStatus.OK);
+    }
+
+    @RequestMapping(
+            path = "/{clientId}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity removeClient(@PathVariable("clientId") Integer clientId){
+        clientMapper.deleteClient(clientId);
+        Map response = new HashMap();
+        response.put("code",0);
+        response.put("message","Eliminación correcta");
+        return new ResponseEntity<>(response,HttpStatus.OK);
+    }
+    public ResponseEntity updateClient(@PathVariable("clientId") Integer clientId,
+                                       @RequestBody ClientDto clientDto) {
+        // Verificamos que el fistName no sea nulo o no sea vacio.
+        if (clientDto.getFirstName() == null || "".equals(clientDto.getFirstName().trim()) ) {
+            Map response = new HashMap();
+            response.put("code", 1000);
+            response.put("message", "El cliente no tiene primer nombre");
+            return new ResponseEntity<Map>(response, HttpStatus.BAD_REQUEST);
+        }
+        // Verificamos que el lastName no sea nulo o no sea vacio.
+        if (clientDto.getLastName() == null || "".equals(clientDto.getLastName().trim())) {
+            Map response = new HashMap();
+            response.put("code", 1000);
+            response.put("message", "El cliente no tiene primer nombre");
+            return new ResponseEntity<Map>(response, HttpStatus.BAD_REQUEST);
+        }
+        // Colocamos el ID en el DTO (porque lo usa el mapper) a partir del PathVariable
+        clientDto.setClientId(clientId);
+        // Invocamos a Base de datos para actualizar cliente.
+        clientMapper.updateClient(clientDto);
+        return new ResponseEntity<>(clientDto, HttpStatus.OK);
     }
 }
--- src/main/java/com/ernesto/backend/ClientMapper.java
+++ src/main/java/com/ernesto/backend/ClientMapper.java
@@ -1,8 +1,32 @@
 package com.ernesto.backend;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.ArrayList;
 
 public interface ClientMapper {
     @Insert("INSERT INTO client (first_name,last_name,birth_date,status) VALUES (#{firstName},#{lastName},#{birthDate},true)")
     void create(ClientDto clientDto);
+
+    @Select(" SELECT client_id as clientId, first_name as firstName, " +
+            " last_name as lastName, birth_date as birthDate, status," +
+            " tx_id as txId, tx_date as txDate, tx_user_id as txUserId, tx_host as txHost " +
+            " FROM client " +
+            " WHERE status = true ")
+    ArrayList<ClientDto> listAll();
+
+    @Delete("UPDATE client SET status = false WHERE client_id = #{clientId}")
+    void deleteClient(Integer clientId);
+
+    @Update( "UPDATE client " +
+            "SET " +
+            "   first_name = #{firstName}," +
+            "   last_name =  #{lastName}," +
+            "   birth_date =  #{birthDate} " +
+            "   WHERE client_id = #{clientId})")
+    void updateClient(ClientDto clientDto);
+
 }

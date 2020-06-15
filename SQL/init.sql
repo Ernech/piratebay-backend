8092f9eaf8e0a5a178174c277acc11cdb0c0680e
Mariana Cardozo <mcardozocosta@hotmail.com>
2020-06-14 23:15:13 -0400

    Queries actualizados

--- SQL/init.sql
+++ SQL/init.sql
@@ -21,6 +21,43 @@ VALUES
 );
 
 
+INSERT INTO "user"
+( user_id, username, password, email, phone_number,
+  cat_user_status, status, tx_id, tx_username, tx_host,
+  tx_date)
+VALUES
+(  nextval('user_user_id_seq') , 'pedrop', '12345', 'pedrop@gmail.com', '70534211',
+   'ACTIVE', 1, 1, 'root', '127.0.0.1',
+   now()
+);
+
+INSERT INTO "user"
+( user_id, username, password, email, phone_number,
+  cat_user_status, status, tx_id, tx_username, tx_host,
+  tx_date)
+VALUES
+(  nextval('user_user_id_seq') , 'lmolina', '12345', 'lmolina@gmail.com', '70642331',
+   'ACTIVE', 1, 1, 'root', '127.0.0.1',
+   now()
+);
+
+INSERT INTO "user"
+( user_id, username, password, email, phone_number,
+  cat_user_status, status, tx_id, tx_username, tx_host,
+  tx_date)
+VALUES
+(  nextval('user_user_id_seq') , 'hugoavila', '12345', 'hugoa@gmail.com', '6771277',
+   'ACTIVE', 1, 1, 'root', '127.0.0.1',
+   now()
+);
+
+/* Borrando features */
+
+UPDATE "user_role"
+SET status = 0
+WHERE user_role_id = 3;
+
+
 INSERT INTO feature (feature_code, feature_name, status, tx_id, tx_username, tx_host, tx_date)
 VALUES ('PAGE_USER_MANAGEMENT', 'Gestión de usuarios', 1, 1, 'admin', 'localhost', now());
 
@@ -29,6 +66,7 @@ VALUES ('PAGE_PRODUCT_MANAGEMENT', 'Gestión de productos', 1, 1, 'admin', 'loca
 
 INSERT INTO feature (feature_code, feature_name, status, tx_id, tx_username, tx_host, tx_date)
 VALUES ('BUTTON_DELETE_USER', 'Botón para eliminar un usuario', 1, 1, 'admin', 'localhost', now());
+
 
 INSERT INTO role ( role_name, status, tx_id, tx_username, tx_host, tx_date)
 VALUES ('ADMIN', 1,  1, 'admin', 'localhost', now());
@@ -64,3 +102,6 @@ VALUES (2, 2, 1,  1, 'admin', 'localhost', now());
 
 INSERT INTO user_role ( user_id, role_id, status, tx_id, tx_username, tx_host, tx_date)
 VALUES (2, 1, 1,  1, 'admin', 'localhost', now());
+
+INSERT INTO user_role ( user_id, role_id, status, tx_id, tx_username, tx_host, tx_date)
+VALUES (2, 3, 1,  1, 'admin', 'localhost', now());
